3f29fde9470c13aeaa92f5e1e95875faa563f9c6
Vishwa1011 <imt_2022119@iiitm.ac.in>
2025-03-30 17:22:10 +0530

    Solved minor issue in OTPverification

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/userController");
+const verifyAccessToken = require('../middleware/verifyAccessToken');
+
+router.use(verifyAccessToken);
 
 router.get("/me", UserController.getCurrentUser);
 router.patch("/me", UserController.updateCurrentUserProfile);
